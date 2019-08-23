932eda458eb4176cb88e18e63144d24ac69f68f6
pushkar.shankar <pushkar.shankar@cgi.com>
2019-08-23 14:26:15 +0530

    Added setter and getter in SentimentReport

--- src/main/java/com/stackroute/sentiment/report/SentimentReport.java
+++ src/main/java/com/stackroute/sentiment/report/SentimentReport.java
@@ -3,12 +3,29 @@ package com.stackroute.sentiment.report;
 import com.stackroute.sentiment.classifier.SentimentClassifier;
 
 public class SentimentReport {
-    public void setSentimentScore(int predictedClass) {
+    double sentimentScore;
+    String sentimentType;
+    SentimentClassifier sentimentClassifier;
+
+    public double getSentimentScore() {
+        return sentimentScore;
     }
 
-    public void setSentimentType(String sentimentType) {
+    public void setSentimentScore(double sentimentScore) {
+        this.sentimentScore = sentimentScore;
     }
 
-    public void setSentimentClass(SentimentClassifier sentimentClassifier) {
+    public String getSentimentType() {
+        return sentimentType;
     }
+
+    public SentimentClassifier getSentimentClassifier() {
+        return sentimentClassifier;
+    }
+
+    public void setSentimentClassifier(SentimentClassifier sentimentClassifier) {
+        this.sentimentClassifier = sentimentClassifier;
+    }
+
+
 }
